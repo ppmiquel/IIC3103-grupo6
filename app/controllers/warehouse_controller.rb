26534b2b6cf2eaf409d50a14116baccd099319df
Pedro Pablo Miquel <ppmiquel@uc.cl>
2016-05-03 21:22:28 -0300

    Bodega

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,10 +1,12 @@
 class WarehouseController < ApplicationController
+
   def consultar
     id = params[:sku]
     stock = getProductStock(id)
     response = { :stock => stock, :sku => id}
     render :json => response
   end
+
   def createHash(data)
     key = 'cd0A9ZK#u#vxES9'
     hmac = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'),key,data)
@@ -30,11 +32,29 @@ class WarehouseController < ApplicationController
   def getProductStock(productId, almacenId)
 
     hash = createHash('GET' + almacenId + productId)
-    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/stock?almacenId=" + almacenId + "&sku=" + productId + "&limit=5").to_s)
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/stock?almacenId=" + almacenId + "&sku=" + productId + "&limit=100").to_s)
 
   end
 
+  def moverStock(productId, almacenId)
 
+    hash = createHash('POST' + productId + almacenId)
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/moverStock?sku=" + productId + "&alamcenId=" + alamcenId + "&limit=100").to_s)
 
+  end
+
+def moverStockBodega(productId, almacenId)
+
+    hash = createHash('POST' + productId + almacenId)
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/moverStockBodega?sku=" + productId + "&alamcenId=" + alamcenId + "&limit=100").to_s)
+
+  end
+
+  def getCuentaFabrica()
+
+    hash = createHash('GET')
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/getCuentaFabrica").to_s)
+
+  end
 
 end
