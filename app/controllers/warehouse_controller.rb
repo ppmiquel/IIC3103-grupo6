11e17cd67923f29f061b904e1085720784c66dd0
Alberto León Salinas <ajleon@uc.cl>
2016-05-03 21:17:17 -0300

    OC Ready

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,38 +1,19 @@
+require 'warehouse_module'
+
 class WarehouseController < ApplicationController
+  include WarehouseModule
+
   def consultar
     id = params[:sku]
-    stock = getProductStock(id)
-    response = { :stock => stock, :sku => id}
+    product_stock = getProductStock(id)
+    response = { :stock => product_stock, :sku => id}
     render :json => response
   end
-  def createHash(data)
-    key = 'cd0A9ZK#u#vxES9'
-    hmac = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'),key,data)
-    hash = Base64.encode64(hmac).chomp
-    return hash
-  end
 
-  def getAlmacenes
 
-    hash = createHash('GET')
 
-    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/almacenes").to_s)
 
-  end
 
-  def getProductsWithStock(almacenId)
-
-    hash = createHash('GET' + almacenId)
-    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/skusWithStock?almacenId=" + almacenId).to_s)
-
-  end
-
-  def getProductStock(productId, almacenId)
-
-    hash = createHash('GET' + almacenId + productId)
-    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/stock?almacenId=" + almacenId + "&sku=" + productId + "&limit=5").to_s)
-
-  end
 
 
 
--- lib/warehouse_module.rb
+++ lib/warehouse_module.rb
@@ -1,6 +1,4 @@
-
 module WarehouseModule
-
   def getProductStock(productId)
 
     stock = 0
@@ -19,5 +17,35 @@ module WarehouseModule
     return stock
 
   end
+
+  def getAlmacenes
+
+    hash = createHash('GET')
+
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/almacenes").to_s)
+
+  end
+
+  def createHash(data)
+    key = 'cd0A9ZK#u#vxES9'
+    hmac = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'),key,data)
+    hash = Base64.encode64(hmac).chomp
+    return hash
+  end
+
+  def getProductsWithStock(almacenId)
+
+    hash = createHash('GET' + almacenId)
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/skusWithStock?almacenId=" + almacenId).to_s)
+
+  end
+
+  def getProductStock2(productId, almacenId)
+
+    hash = createHash('GET' + almacenId + productId)
+    return JSON.parse(HTTP.headers(:"Content-Type" => "application/json", :Authorization => "INTEGRACION grupo6:" + hash).get("http://integracion-2016-dev.herokuapp.com/bodega/stock?almacenId=" + almacenId + "&sku=" + productId + "&limit=5").to_s)
+
+  end
+
 
 end
