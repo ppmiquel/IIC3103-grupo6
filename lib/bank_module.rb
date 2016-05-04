3e37d6c3fee355f0c24aba372e1ae898f47fbaef
Tomas Andrews <jtandrew@uc.cl>
2016-05-03 22:57:50 -0300

    metodos banco

--- lib/bank_module.rb
+++ lib/bank_module.rb
@@ -1,11 +1,26 @@
 module BankModule
-  def getTrx(idtrx)
-
+	
+  def obtenerTransaccion(idtrx)
     trx= JSON.parse(HTTP.headers(:"Content-Type" => "application/json").get("http://mare.ing.puc.cl/banco/trx/"+idtrx,).to_s, :symbolize_names => true)
     return trx
   end
 
-  def getTrxValidation(trx)
+  def ValidacionTransaccion(trx)
     return true
+  end
+
+  def transferir(monto, origen, destino)
+  	trx= JSON.parse(HTTP.headers(:"Content-Type" => "application/json").put("http://mare.ing.puc.cl/trx", :params => {:monto => monto, :origen => origen, :destino => destino} ).to_s, :symbolize_names => true)
+  	return trx
+  end
+
+  def obtenerCartola(fechaInicio, fechaFin, id)
+  	cartola = JSON.parse(HTTP.headers(:"Content-Type" => "application/json").post("http://mare.ing.puc.cl/cartola"+id, :params => {:fechaInicio => fechaInicio , :fechaFin => fechaFin, :id => id}).to_s, :symbolize_names => true)
+  	return cartola
+  end
+
+  def obtenerCuenta(id)
+	cuenta = JSON.parse(HTTP.headers(:"Content-Type" => "application/json").get("http://mare.ing.puc.cl/banco/cuenta/"+id,).to_s, :symbolize_names => true)
+	return cuenta
   end
 end
