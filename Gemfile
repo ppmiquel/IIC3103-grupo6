7eebe3d4bfd9a45f297150a0d965ed086841d749
Alicia del Sol <adelsol1@uc.cl>
2016-05-07 17:59:20 -0300

    moto

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem "unicorn"
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 
-gem  'net/sftp'
+
 gem  'net/ssh'
 gem  'nokogiri'
 gem  'bundle'
--- lib/sftp_oc_module.rb
+++ lib/sftp_oc_module.rb
@@ -1,15 +1,15 @@
 
 
-    require 'net/sftp'
+
 
 
 def saveSftpOc()
   #nos conectamos
-  session = Net::SFTP.start('moto.ing.puc.cl', 'integra6', :password => '8s9eDblt', :port =>22)
-  sftp = Net::SFTP::Session.new(session)
-    sftp.dir.foreach("/pedidos") do |archivo|
+  #session = Net::SFTP.start('moto.ing.puc.cl', 'integra6', :password => '8s9eDblt', :port =>22)
+  #sftp = Net::SFTP::Session.new(session)
+  #  sftp.dir.foreach("/pedidos") do |archivo|
 
-      myfile.csv /my/dir/myfile.csv
+  #    myfile.csv /my/dir/myfile.csv
 
 
 end
