1aa133d7374f56d817f1c11a7bca1f525b6a9467
Alicia del Sol <adelsol1@uc.cl>
2016-05-31 15:56:35 -0300

    sftp

--- lib/sftp_oc_module.rb
+++ lib/sftp_oc_module.rb
@@ -1,1 +1,15 @@
 
+def leer_sftp
+  session = Net::SSH.start('mare.ing.puc.cl', 'integra6', password: 'BSnt6txv', port: 22)
+  sftp = Net::SFTP::Session.new(session)
+  sftp.connect!
+  sftp.dir.foreach('/pedidos') do |archivo|
+    if((archivo.name).end_with?('.xml'))
+      file = sftp.download!("/pedidos/"<<archivo.name)
+      doc = Nokogiri::XML(file)
+      orden = obtenerOC(doc.css("id").map.first.children.text);
+      Orden.create(idoc: orden[0][:_id], canal:orden[0][:canal], cliente: orden[0][:cliente], sku: orden[0][:sku], cantidad: orden[0][:cantidad], precio:orden[0][:precioUnitario], :estado => 'leida', fecha_entrega: (orden[0][:fechaEntrega]).to_i )
+      sftp.rename("/pedidos/"<<archivo.name, "/pedidos/leidos/"<<archivo.name)
+    end
+  end
+end
