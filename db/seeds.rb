a5f2ad7c0d94212a8506baeecc8b62028051b78b
Alicia del Sol <adelsol1@uc.cl>
2016-06-06 00:24:20 -0300

    ahora si?

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,12 +50,6 @@ productos_list = [
 productos_list.each do |name, description, available_on, meta_keywords, price, image_name, sku|
 	product = Spree::Product.create(sku: sku, cost_currency: "CLP", name: name, description: description, available_on: available_on, meta_keywords: meta_keywords, tax_category_id: 1, shipping_category_id: 1, promotionable: false, price: price)
 	path = 'public/spree/products/' + meta_keywords + '/product/' + image_name
-   id = Spree::Variant.find_by(product_id: product.id).id
- 	i = Spree::Image.create!(attachment: File.open(path), viewable_type: "Spree::Variant", viewable_id: id, attachment_file_name: image_name)
-   product.images << i
-   product.save
-   path = 'public/spree/products/' + meta_keywords + '/product/' + image_name
-   image = Spree::Image.create(attachment: File.open(path), viewable: product.master, viewable_id: product.id, viewable_type: 'Spree::Variant', attachment_file_name: image_name , type: "Spree::Image")
- image.save
+
 product.save
 end
