e17cbd153749f7d1f48fa9dcf752fe970b0c52c1
Alicia del Sol <adelsol1@uc.cl>
2016-06-25 19:43:59 -0300

    con formateo de direccion y claves en desarrollo en spree

--- app/models/banco.rb
+++ app/models/banco.rb
@@ -3,9 +3,9 @@
 class Banco < ActiveRecord::Base
 
 def self.crearBoleta(total)
-  idproveedor='572aac69bdb6d403005fb047'
+  idproveedor= $idGrupo
   cliente='lala'
-  boleta= JSON.parse(HTTP.headers(:"Content-Type" => "application/json").put("http://moto.ing.puc.cl/facturas/boleta", :json => {:proveedor => idproveedor,:cliente=>cliente,:total=> total}).to_s, :symbolize_names => true)
+  boleta= JSON.parse(HTTP.headers(:"Content-Type" => "application/json").put("http://"+$url+"/facturas/boleta", :json => {:proveedor => idproveedor,:cliente=>cliente,:total=> total}).to_s, :symbolize_names => true)
   # idbol=boleta[:_id]
   # cliente =boleta[:cliente]
   #
